37714aae8a89c4aba5119390e790b4f284730c01
Douglas Campbell <dacampbelltbone@gmail.com>
2020-02-01 00:40:37 -0500

    Complete recursive merge sort. Tests have passed. MVP met.

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,44 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
+def merge(arrA, arrB):
+    # init an empty list
+    merged_arr = []
+    # set index to 0
+    idx = jdx = 0
+
+    # loop while index is < len of [A] and j index is < len of [B]
+    while idx < len(arrA) and jdx < len(arrB):
+        # if A[idx] is < B[jdx]
+        if arrA[idx] < arrB[jdx]:
+            # append A[idx] to merged_arr
+            merged_arr.append(arrA[idx])
+            # increment idx
+            idx += 1
+        else:
+            # append B[jdx] to merged_arr
+            merged_arr.append(arrB[jdx])
+            # increment jdx
+            jdx += 1
+    # merge lists
+    merged_arr += arrA[idx:]
+    merged_arr += arrB[jdx:]
+
     return merged_arr
 
 
-# TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+def merge_sort(arr):
+    # if list len is < 2
+    if len(arr) < 2:
+        # return list
+        return arr
+    # find middle of list
+    midArr = len(arr) // 2
+    # split list from beginning to middle
+    arrL = merge_sort(arr[:midArr])
+    # spilt list from middle to end
+    arrR = merge_sort(arr[midArr:])
 
-    return arr
+    # merge list
+    return merge(arrL, arrR)
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +47,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +56,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
