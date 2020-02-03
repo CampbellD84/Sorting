6da233c6acc671fe8c0381c2324a41ba4064da6c
Douglas Campbell <dacampbelltbone@gmail.com>
2020-02-03 04:17:29 -0500

    Complete stretch for search algorithms, linear and binary. All tests have passed.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,46 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+    for idx in range(0, len(arr)):
+        if arr[idx] == target:
+            return idx
 
-   return -1   # not found
+    return -1   # not found
 
 
-# STRETCH: write an iterative implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    if len(arr) == 0:
+        return -1  # array empty
 
-  # TO-DO: add missing code
+    low = 0
+    high = len(arr)-1
 
-  return -1 # not found
+    while low <= high:
+        mid = (low + high) // 2
+        if target < arr[mid]:
+            # get rid of right hand side
+            high = mid-1
+        elif target > arr[mid]:
+            # get rid of left hand side
+            low = mid+1
+        else:
+            return mid
+
+    return -1  # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
+# STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    mid = (low+high)//2
+
+    if len(arr) == 0:
+        return -1  # array empty
+
+    if target == arr[mid]:
+        return mid
+    elif target < arr[mid]:
+        return binary_search_recursive(arr[:mid], target, 0, len(arr[:mid]))
+    else:
+        return binary_search_recursive(arr[mid:], target, 0, len(arr[mid:])-1)
